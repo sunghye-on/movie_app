ea65193bcaa26fcc09e310b7f6806cf4d346d811
tjdgus <opei71@naver.com>
2019-09-03 11:46:29 +0000

    first

---
+++ src/Movie.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import "./Movie.css";
+function Movie({ year , title ,summary ,poster, genres}){
+    return (
+        <div className = "movie">
+            <img src = {poster} alt ="poster"></img>
+            <div>
+            <h1 className = "m_title">{title}</h1>
+            <h4 className = "m_year">{year}</h4>
+            <ul className = "genres">
+                {genres.map((genres, number)=>(
+                <li key = {number}>{genres}</li>
+                ))}
+            </ul>
+            <p className = "m_summary">{summary}</p>
+            </div>
+        </div>
+    );
+}
+
+Movie.propTypes = {
+	id : PropTypes.number.isRequired,
+	year : PropTypes.number.isRequired,
+	title : PropTypes.string.isRequired,
+	summary : PropTypes.string.isRequired,
+	poster : PropTypes.string.isRequired,
+    genres : PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
