92bcac7d4d4e28def438f915d6d106d74ce895c6
tjdgus <opei71@naver.com>
2019-09-03 11:53:46 +0000

    Updates

---
+++ precache-manifest.3fae5e97b38a45cef94c447eb17e3639.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "de2366ee33832e761ac4",
+    "url": "/movie_app/static/js/main.de2366ee.chunk.js"
+  },
+  {
+    "revision": "b274923d75a90bdb8204",
+    "url": "/movie_app/static/js/1.b274923d.chunk.js"
+  },
+  {
+    "revision": "de2366ee33832e761ac4",
+    "url": "/movie_app/static/css/main.15bbd65c.chunk.css"
+  },
+  {
+    "revision": "b56a4f14c5aed0ec2e02a3aafb059a31",
+    "url": "/movie_app/index.html"
+  }
+];
